bc47ecb337865c42d774ea327b709281add63840
glisom <grantisom@ku.edu>
2014-12-13 15:25:44 -0600

    added core data and save feature, sort of

---
+++ ScheduleCheck/Classes.swift
@@ -0,0 +1,17 @@
+//
+//  Classes.swift
+//  ScheduleCheck
+//
+//  Created by Grant Isom on 12/13/14.
+//  Copyright (c) 2014 Grant Isom. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@objc(Classes)
+class Classes: NSManagedObject {
+    @NSManaged var classname:String
+     
+    
+}
--- ScheduleCheck/ViewController.swift
+++ ScheduleCheck/ViewController.swift
@@ -7,9 +7,73 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var className: UITextField!
+    
+    @IBOutlet weak var startTime1: UITextField!
+    @IBOutlet weak var startTime2: UITextField!
+    @IBOutlet weak var startTime3: UITextField!
+    
+    @IBOutlet weak var endTime1: UITextField!
+    @IBOutlet weak var endTime2: UITextField!
+    @IBOutlet weak var endTIme3: UITextField!
+    
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
+    
+    @IBOutlet weak var monSwitch: UISwitch!
+    @IBOutlet weak var tuesSwitch: UISwitch!
+    @IBOutlet weak var wedSwtich: UISwitch!
+    @IBOutlet weak var thursSwitch: UISwitch!
+    @IBOutlet weak var friSwitch: UISwitch!
+    
+    @IBAction func createClicked(sender: AnyObject) {
+        
+        let appDel:AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        let context:NSManagedObjectContext = appDel.managedObjectContext!
+        
+        let ent = NSEntityDescription.entityForName("Classes", inManagedObjectContext: context)
+        
+        var newClass = Classes(entity: ent!, insertIntoManagedObjectContext: context)
+        newClass.classname = className.text
+        //newClass.time1 = startTime1.text
+        
+        context.save(nil)
+        
+        println(newClass)
+        println("Obj saved")
+        
+    }
+    
+    @IBAction func doneClicked(sender: AnyObject) {
+        let appDel:AppDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        let context:NSManagedObjectContext = appDel.managedObjectContext!
+        
+        let request = NSFetchRequest(entityName: "Classes")
+        request.returnsObjectsAsFaults = false;
+        
+        request.predicate = NSPredicate(format: "classname = %@", className.text)
+        
+        var results:NSArray = context.executeFetchRequest(request, error: nil)!
+        
+        if results.count > 0 {
+            for name in results {
+                var thisClassName = name as Classes
+                println("the class name is \(thisClassName.classname)")
+            }
+            println("\(results.count) found")
+        } else {
+            println ("No Results found")
+        }
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +83,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        self.view.endEditing(true);
+        return true;
+    }
+    
 
 }
 
